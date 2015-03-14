d9e7e5ff18caa6cc46d26274ba910fe82847ed93
Jeremy Jones <banjeremy@gmail.com>
2015-03-14 17:00:16 -0500

    read radii from pi digits

--- app/index.js
+++ app/index.js
@@ -1,27 +1,26 @@
 var img;
-var smallPoint, largePoint;
-var pi = '3.14159265359';
-
-function windowSize() {
-
-}
-
+var index = 0;
+var pi = '3.14159265358979323846264338';
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(window.innerWidth, window.innerHeight);
   img = loadImage('img/eso1510a.jpg');
   imageMode(CENTER);
   noStroke();
   background(255);
   frameRate(999);
+  // loadJSON('/data/pi.json', function(data){
+  //   debugger;
+  // });
 }
 
 function draw() {
-  var pointillize = ;
-
+  index = ++index % pi.length;
+  var radius = parseInt(pi.charAt(index));
+  radius *= 2;
   var x = floor(random(img.width));
   var y = floor(random(img.height));
   var pix = img.get(x, y);
   fill(pix, 128);
-  ellipse(x, y, pointillize, pointillize);
+  ellipse(x, y, radius, radius);
 }
