dd1694c1ec05cd1683f1995a39cd5ca51614f4bf
Kayla Jones <kaylalyne424@gmail.com>
2015-03-14 17:00:37 -0500

    working with Text

--- app/index.js
+++ app/index.js
@@ -1,23 +1,30 @@
 var img;
 var smallPoint, largePoint;
 var pi = '3.14159265359';
+var greeting = 'Time is a flat Circle.';
 
 function windowSize() {
 
 }
 
-
 function setup() {
+  noCursor();
   createCanvas(720, 400);
   img = loadImage('img/eso1510a.jpg');
   imageMode(CENTER);
   noStroke();
   background(255);
   frameRate(999);
+  fill(0);
+  text(greeting, 175, 175, 1000, 1000);
+  textSize(150);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  textFont("Helvetica");
 }
 
 function draw() {
-  var pointillize = ;
+  var pointillize = 3;
 
   var x = floor(random(img.width));
   var y = floor(random(img.height));
